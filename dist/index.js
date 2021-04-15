127940c7eba7521b5b30caff40dae54ea531168a
Denys Zheleznyi <denys.zheleznyi@chisw.com>
2021-04-15 10:39:45 +0300

    feature/auth, whitespaces trim fix

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ class FormValidator {
         this.validateOnSubmit()
         this.validateOnEntry()
         this.unlockButton()
+        this.showPassword()
     }
 
     validateOnSubmit() {
@@ -25,6 +26,7 @@ class FormValidator {
             const input = document.querySelector(`#${field}`)
             input.addEventListener('input', e => {
                 this.validateFields(input)
+
             })
             input.addEventListener('select', e => {
                 this.validateFields(input)
@@ -33,39 +35,34 @@ class FormValidator {
     }
 
     validateFields(input) {
+        input.type === 'password' ? input.value = input.value.trim().replace(/\s+/g, '') : input.value = input.value.trimStart().replace(/\s{2,}/g, ' ')
+
         let div = input.parentElement
         let span = input.parentElement.nextSibling
+        let value = input.value
 
-        if (input.value.trim().length < 1) {
+        if (value.length < 1) {
             span.innerHTML = 'Required field'
             div.style.border = '1px solid red';
             div.style.marginBottom = '9px';
             div.style.borderRadius = '7px'
-
-        } else if (input.value === 'country') {
+        } else if (value === 'country') {
             span.innerHTML = 'Required field'
             div.style.border = '1px solid red';
             div.style.marginBottom = '9px';
-            div.style.borderRadius = '7px'
-        }
-        else if (input.value !== 'country' && input.id === 'countries') {
+            div.style.borderRadius = '7px';
+        } else if (value !== 'country' && input.id === 'countries') {
             div.removeAttribute('style')
             span.innerHTML = ''
-        }
-        else if (input.value.length > 0 && input.id === 'name') {
+        } else if (value.length > 0 && input.id === 'name') {
             div.removeAttribute('style')
             span.innerHTML = ''
-        } else if (input.id === 'password' && input.length < 8) {
+        } else if (input.id === 'password' && input.value.length < 8) {
             span.innerHTML = 'Password should be at least 8 symbols long'
             div.style.border = '1px solid red';
             div.style.marginBottom = '9px';
             div.style.borderRadius = '7px'
-        } else if (input.id === 'password' && input.value.trim().length < 8) {
-            span.innerHTML = 'Password should be at least 8 symbols long'
-            div.style.border = '1px solid red';
-            div.style.marginBottom = '9px';
-            div.style.borderRadius = '7px'
-        } else if ((input.id === 'password' && input.value.trim().length > 7)) {
+        } else if ((input.id === 'password' && input.value.length > 7)) {
             span.innerHTML = ''
             div.removeAttribute('style')
         }
@@ -73,17 +70,33 @@ class FormValidator {
 
     unlockButton() {
         let terms = document.getElementById('terms')
-        let button =  document.querySelector('button')
+        let button = document.querySelector('button')
 
         terms.addEventListener('change', e => {
             if (terms.checked) {
                 button.disabled = false
                 button.style.backgroundColor = 'dodgerblue'
-            }
-            else {
+            } else {
                 button.disabled = false
                 button.style.backgroundColor = 'silver'
             }
+        })
+    }
+
+    showPassword() {
+        let eye = document.getElementById("eye")
+        let eye_slashed = document.getElementById("eye-slashed")
+        let password = document.getElementById("password")
+
+        eye.addEventListener('click', () => {
+            password.attributes["type"].value = 'text'
+            eye.style.display = 'none'
+            eye_slashed.style.display = 'block'
+        })
+        eye_slashed.addEventListener('click', () => {
+            password.attributes["type"].value = 'password'
+            eye.style.display = 'block'
+            eye_slashed.style.display = 'none'
         })
     }
 }
@@ -98,15 +111,3 @@ const validator = new FormValidator(form, fields)
 validator.initialize()
 
 
-document.getElementById("eye").onclick = () => {
-    document.getElementById("password").attributes["type"].value = 'text'
-    document.getElementById('eye').style.display = 'none'
-    document.getElementById('eye-slashed').style.display = 'block'
-
-}
-document.getElementById("eye-slashed").onclick = () => {
-    document.getElementById("password").attributes["type"].value = 'password'
-    document.getElementById('eye-slashed').style.display = 'none'
-    document.getElementById('eye').style.display = 'block'
-
-}
--- src/js/index.js
+++ src/js/index.js
@@ -6,15 +6,3 @@ const validator = new FormValidator(form, fields)
 validator.initialize()
 
 
-document.getElementById("eye").onclick = () => {
-    document.getElementById("password").attributes["type"].value = 'text'
-    document.getElementById('eye').style.display = 'none'
-    document.getElementById('eye-slashed').style.display = 'block'
-
-}
-document.getElementById("eye-slashed").onclick = () => {
-    document.getElementById("password").attributes["type"].value = 'password'
-    document.getElementById('eye-slashed').style.display = 'none'
-    document.getElementById('eye').style.display = 'block'
-
-}
